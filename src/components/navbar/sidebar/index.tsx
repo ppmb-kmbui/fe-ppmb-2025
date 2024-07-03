0cb44b0697f4c354b7185bafb19ce6167142ee16
sunsetheus <williamsalimrjs@gmail.com>
2024-07-03 20:41:00 +0700

    style: fix sidebar visibility for login and signup page

--- src/components/navbar/sidebar/index.tsx
+++ src/components/navbar/sidebar/index.tsx
@@ -22,7 +22,7 @@ export const Sidebar: React.FC = () => {
     ];
 
     return (
-        <div className={`${isExpanded ? "block w-[250px] px-4" : "w-[65px] px-[10px]"} pt-4 gap-3 h-screen hidden md:flex bg-white flex-col duration-300 ease-in-out items-center`} id="bar-fixed" ref={ref}>
+        <div className={`${pathname == "/signup" || pathname == "/login" ? "hidden" : "md:flex"} ${isExpanded ? "block w-[250px] px-4" : "w-[65px] px-[10px]"} pt-4 gap-3 h-screen hidden bg-white flex-col duration-300 ease-in-out items-center`} id="bar-fixed" ref={ref}>
             <div className="h-[60px] flex items-center cursor-pointer" onClick={() => setIsExpanded(!isExpanded)}>
                 <Image
                     src={isExpanded ? "/image/logo-full.png" : "/image/logo.png"}
@@ -37,7 +37,7 @@ export const Sidebar: React.FC = () => {
             <div className="flex flex-col gap-[8px] w-full h-full justify-between">
                 <div className="flex flex-col gap-[8px] w-full h-full">
                     {MAIN_MENU.map((menu, i) => (
-                        <div key={i} className={`${pathname == menu.route ? "bg-ppmb-blue-500 text-ppmb-000" : "hover:bg-ppmb-100 text-ppmb-800"} ${!isExpanded && "justify-center"} group flex items-center px-3 py-2 rounded-lg cursor-pointer`} onClick={() => router.push(menu.route)}>
+                        <div key={i} className={`${pathname == menu.route ? "bg-ppmb-blue-500 text-ppmb-000" : "hover:bg-ppmb-100 text-ppmb-800"} ${!isExpanded && "justify-center"} group flex items-center px-3 py-2 rounded-lg cursor-pointer`} onClick={() => {router.push(menu.route), setIsExpanded(false)}}>
                             <div className="flex flex-row gap-3">
                                 <text className="text-[26px]">{menu.icon}</text>
                                 <text className={`${isExpanded ? "text-[18px]" : "absolute transition-transform delay-500 opacity-0 translate-x-28 overflow-hidden pointer-events-none"} whitespace-pre duration-500 font-medium`} style={{transitionDelay: `${i + 3}00ms`}}>{menu.text}</text>
