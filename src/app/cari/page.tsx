33a91ed59a8b42e3a136f883c8e1df326a67d21d
sunsetheus <williamsalimrjs@gmail.com>
2024-07-17 00:19:17 +0700

    feat: add loader

--- src/app/cari/page.tsx
+++ src/app/cari/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Button, UserCard, Input, SearchBar } from "@/components";
+import { Button, UserCard, Input, SearchBar, Loader } from "@/components";
 import withAuth from "@/hoc/withAuth";
 import { api } from "@/utils/axios";
 import debounce from "debounce";
@@ -21,63 +21,27 @@ interface UserProps {
 const CariPage: React.FC = () => {
     const [message, setMessage] = useState<string>("");
     const [friends, setFriends] = useState<UserProps[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const searchParams = useSearchParams();
     const router = useRouter();
 
-    const DATA = [
-        {
-            name: "Ariana Grande",
-            faculty: "FEB, 2024"
-        },
-        {
-            name: "Lana del Rey",
-            faculty: "FIB, 2024"
-        },
-        {
-            name: "Billie Eilish",
-            faculty: "FT, 2024"
-        },
-        {
-            name: "Jennie BLACKPINK",
-            faculty: "FEB, 2024"
-        },
-        {
-            name: "Stephen Sanchez",
-            faculty: "Fasilkom, 2024"
-        },
-        {
-            name: "Taylor Swift",
-            faculty: "FH, 2024"
-        },
-        {
-            name: "DJ Python Typescript Yeehaw",
-            faculty: "FF, 2024"
-        },
-        {
-            name: "Nadin Amizah",
-            faculty: "Fpsi, 2024"
-        },
-        {
-            name: "NIKI",
-            faculty: "FEB, 2024"
-        },
-    ]
-
     const getData = async () => {
         try {
+            setIsLoading(true);
             const queryString = new URLSearchParams(searchParams).toString();
-            console.log(queryString, "ini kueri")
+
             const res = await api({
                 method: 'GET',
                 url: `api/friends?${queryString}`
             })
 
-            console.log(res, "ini res");
             setFriends(await res.data.friends);
 
         } catch (error: any) {
             console.error("Error in getting friends data")
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -95,10 +59,8 @@ const CariPage: React.FC = () => {
         getData();
     }, [searchParams]);
 
-    console.log(friends)
-
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col items-center gap-10">
             <div className="bg-gradient-to-r from-ppmb-blue-600 to-ppmb-blue-300 px-[30px] md:px-[100px] flex flex-col py-10 gap-3 items-center w-full">
                 <div className="flex text-ppmb-800 justify-center items-center text-xl md:text-3xl lg:text-4xl gap-2 font-semibold">
                     <text className="text-ppmb-000">NETWORKING</text>
@@ -106,7 +68,6 @@ const CariPage: React.FC = () => {
                     <text>KMB</text>
                 </div>
 
-                {/* <Input placeholder="Cari teman KMB" setValue={setSearchQuery} type="rounded" icon={<HiSearch />} /> */}
                 <SearchBar handleSearch={handleSearch}/>
 
                 <div className="text-white flex flex-col items-center text-center mt-2 md:mt-4">
@@ -115,15 +76,21 @@ const CariPage: React.FC = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 mt-3 md:mt-5 lg:mt-7 gap-3 md:gap-7 lg:gap-7 px-3 md:px-5 lg:px-7">
-                {friends?.map((data, key) => (
-                    <UserCard key={key} {...data}/>
-                ))}
-            </div>
+            {isLoading ? 
+                <Loader />
+                : 
+                <>
+                    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-3 md:gap-7 lg:gap-7 px-3 md:px-5 lg:px-7">
+                        {friends?.map((data, key) => (
+                            <UserCard key={key} {...data}/>
+                        ))}
+                    </div>
 
-            <div className={`${friends.length == 0 ? 'flex' : 'hidden'} justify-center items-center mt-3 italic text-2xl text-ppmb-500`}>Tidak ditemukan teman dengan nama tersebut!</div>
+                    <div className={`${friends.length == 0 ? 'flex' : 'hidden'} justify-center items-center mt-3 italic md:text-2xl text-ppmb-500`}>Tidak ditemukan teman dengan nama tersebut!</div>
+                </>
+            }
 
-            <div className="flex flex-col items-center py-14 gap-[2px] md:gap-1 w-full px-8 lg:px-[100px]">
+            <div className="flex flex-col items-center gap-[2px] md:gap-1 w-full px-8 lg:px-[100px]">
                 <text className="text-lg md:text-2xl font-semibold">Kirim pesan untuk teman-teman KMBUI kamu!</text>
                 <div className="flex gap-2 md:gap-4 items-center w-full justify-center">
                     <Input icon={<HiOutlineChat />} placeholder="Kirim pesanmu!" setValue={setMessage} type="rounded" />
