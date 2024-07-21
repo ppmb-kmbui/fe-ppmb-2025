7dd279bf979500b891f680335a2487905b9129f7
sunsetheus <williamsalimrjs@gmail.com>
2024-07-21 15:10:14 +0700

    feat: add assignments

--- src/app/tugas/page.tsx
+++ src/app/tugas/page.tsx
@@ -34,8 +34,8 @@ interface ProgressProps {
     networkingAngkatan: NetworkingAngkatanProgressProps
     networkingKating: NetworkingKatingProgressProps
     firstFossibDone: boolean
-    secondFosibDone: boolean
-    insightHuntingDone: true
+    secondFossibDone: boolean
+    insightHuntingDone: boolean
 }
 
 interface AssingmentProps {
@@ -44,9 +44,10 @@ interface AssingmentProps {
     deadline: string
     icon: React.ReactNode
     isFinished: boolean
+    type: "file" | "input" | "image"
     namingFormat?: string
-    type: "file" | "input" | "networking"
     template?: string
+    rsvp?: string
     vbg?: string
 }
 
@@ -80,8 +81,6 @@ const TugasPage: React.FC = () => {
         getData();
     }, [])
 
-    console.log(progress);
-
     const DUMMY_DATA_PROGRESS = [
         {
             label: "Mentoring",
@@ -107,57 +106,100 @@ const TugasPage: React.FC = () => {
 
     const ASSINGMENTS: AssingmentProps[] = [
         {
+            name: "Insight Hunting",
+            description: "Melalui Insight Hunting, diharapkan maba mendapatkan wawasan dari narasumber yang berpengalaman di beberapa kategori yang diminati.",
+            deadline: "29/08/2024",
+            icon: <HiOutlineLightBulb />,
+            isFinished: false,
+            namingFormat: "Nama Lengkap_Fakultas_FosterSibling2",
+            type: "file",
+            rsvp: "https://docs.google.com/forms/d/e/1FAIpQLScIHbDL7w-SaY_sDy-z7wabRaBLVKIRZ3CfwIQuJEuQD73S7Q/viewform",
+            template: "https://docs.google.com/document/d/1Z3engQq3QwqyE2HjhTSxwxJchf2brPr7/edit",
+            vbg: "https://drive.google.com/file/d/18QDIWk0txOuxGM6OvyFeOjTdo8urAyC6/view?usp=drive_link"
+        },
+        {
             name: "Fossib: Sharing Insight",
             description: "Maba dan kakak asuh melakukan sharing bersama untuk dapat saling mengenal dan bertukar wawasan mengenai kehidupan perkuliahan.",
             deadline: "29/08/2024",
             icon: <HiOutlineUsers />,
             isFinished: progress.firstFossibDone,
             namingFormat: "Nama Lengkap_Fakultas_FosterSibling1",
-            type: "file"
+            type: "file",
+            template: ""
         },
-        // {
-        //     name: "Insight Hunting",
-        //     description: "Ini bukan deskripsi insight hunting sebenarnya, Ini bukan deskripsi insight hunting sebenarnya",
-        //     deadline: "23/07/2024",
-        //     template: "",
-        //     icon: <HiOutlineLightBulb />,
-        //     type: "file",
-        //     // isFinished: false
-        // },
-        // {
-        //     name: "Foster Sibling",
-        //     description: "Ini juga bukan deskripsi insight hunting sih.., Ini juga bukan deskripsi insight hunting sih..",
-        //     deadline: "23/07/2024",
-        //     template: "",
-        //     icon: <HiOutlineUsers />,
-        //     type: "file",
-        //     // isFinished: true
-        // },
-        // {
-        //     name: "Mentoring 3",
-        //     description: "Mentoring tiga, kalo dua jadi mentoring dua, Mentoring tiga, kalo dua jadi mentoring dua",
-        //     deadline: "23/07/2024",
-        //     icon: <HiOutlineDocumentText />,
-        //     type: "file",
-        //     // isFinished: true
-        // },
-        // {
-        //     name: "Mentoring 4 [VLOG]",
-        //     description: "Mentoring empat, kalo dua jadi mentoring dua, Mentoring empat, kalo dua jadi mentoring dua",
-        //     deadline: "23/07/2024",
-        //     icon: <HiOutlineDocumentText />,
-        //     type: "input",
-        //     // isFinished: true
-        // },
-        // {
-        //     name: "Mentoring 4 [PPT]",
-        //     description: "Mentoring empat, kalo dua jadi mentoring dua, Mentoring empat, kalo dua jadi mentoring dua",
-        //     deadline: "23/07/2024",
-        //     template: "",
-        //     icon: <HiOutlineDocumentText />,
-        //     type: "file",
-        // },
+        {
+            name: "Fossib: Fun Activity",
+            description: "Setelah sharing insight, maba dan kakak asuh melakukan kegiatan bersama untuk dapat saling mendekatkan diri.",
+            deadline: "29/08/2024",
+            icon: <HiOutlineUsers />,
+            isFinished: progress.secondFossibDone,
+            namingFormat: "Nama Lengkap_Fakultas_FosterSibling2",
+            type: "file",
+            template: ""
+        },
+        {
+            name: "Networking Kating 2023",
+            description: "Maba bersama teman satu kelompok networking menjalin relasi dengan kakak tingkat KMBUI angkatan 2023.",
+            deadline: "31/08/2024",
+            icon: <HiOutlineChatAlt2 />,
+            isFinished: false,
+            namingFormat: "[Nama Lengkap]_[Fakultas]_Networking2023_[nomor].pdf",
+            type: "file",
+            template: ""
+        },
+        {
+            name: "Networking Kating 2022",
+            description: "Maba bersama teman satu kelompok networking menjalin relasi dengan kakak tingkat KMBUI angkatan 2022.",
+            deadline: "31/08/2024",
+            icon: <HiOutlineChatAlt2 />,
+            isFinished: false,
+            namingFormat: "[Nama Lengkap]_[Fakultas]_Networking2022_[nomor].pdf",
+            type: "file",
+            template: ""
+        },
+        {
+            name: "Networking Kating 2021",
+            description: "Maba bersama teman satu kelompok networking menjalin relasi dengan kakak tingkat KMBUI angkatan 2021.",
+            deadline: "31/08/2024",
+            icon: <HiOutlineChatAlt2 />,
+            isFinished: false,
+            namingFormat: "[Nama Lengkap]_[Fakultas]_Networking2021_[nomor].pdf",
+            type: "file",
+            template: ""
+        },
+        {
+            name: "Mentoring: Self Reflection",
+            description: "Setelah tiap sesi mentoring, maba diharapkan dapat menulis rangkuman intisari dari kegiatan yang dilakukan sesuai kreativitas masing-masing.",
+            deadline: "11/09/2024",
+            icon: <HiOutlineDocumentText />,
+            isFinished: false,
+            namingFormat: "[Nama Lengkap]_[Fakultas]_SelfReflection",
+            type: "image",
+            template: ""
+        },
+        {
+            name: "Mentoring: Vlog",
+            description: "Vlog berisi  cuplikan kegiatan selama mentoring dengan durasi maksimal 3 menit, dikumpulkan oleh ketua kelompok.",
+            deadline: "11/09/2024",
+            icon: <HiOutlineDocumentText />,
+            isFinished: false,
+            namingFormat: "[Nomor Kelompok]_[Nama Kelompok]_Vlog",
+            type: "input",
+        },
+        {
+            name: "KMBUI Explorer",
+            description: "Maba mengikuti paling sedikit 2 proker yang diadakan oleh KMBUI agar lebih mengenali KMBUI dan nilai-nilai Buddhis.",
+            deadline: "11/09/2024",
+            icon: <HiOutlineUsers />,
+            isFinished: false,
+            namingFormat: "[Nama Lengkap]_[Fakultas]_KMBUIExplorer",
+            type: "image",
+            template: ""
+        },
     ]
+
+    // console.log(progress);
+    // console.log(ASSINGMENTS);
 
     return (
         isFetching ? <LoadingScreen /> : 
@@ -181,19 +223,23 @@ const TugasPage: React.FC = () => {
                 <div className="flex flex-col gap-2">
                     <text className="text-2xl lg:text-[27px] lg:leading-[1.6] font-semibold">Belum Dikerjakan</text>
 
-                    <div className="flex flex-col gap-5">
-                        {ASSINGMENTS.map((data, key) => (
-                            <TaskCard {...data}/>
+                    <div className="flex flex-col gap-5 w-full">
+                        {ASSINGMENTS.map((assignment, key) => (
+                            <div className={`${assignment.isFinished == true && "hidden"}`}>
+                                <TaskCard key={key} {...assignment}/>
+                            </div>
                         ))}
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 w-full">
                     <text className="text-2xl lg:text-[27px] lg:leading-[1.6] font-semibold">Sudah Dikumpulkan</text>
 
                     <div className="flex flex-col gap-5">
-                        {ASSINGMENTS.map((data, key) => (
-                            <TaskCard {...data}/>
+                        {ASSINGMENTS.map((assingment, key) => (
+                            <div className={`${assingment.isFinished == false && "hidden"}`}>
+                                <TaskCard key={key} {...assingment}/>
+                            </div>
                         ))}
                     </div>
                 </div>
