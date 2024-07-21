c8ed5324e80179b369057d7d68cd160222f4b1ba
sunsetheus <williamsalimrjs@gmail.com>
2024-07-22 00:55:39 +0700

    feat: add timeline data

--- src/components/home/Timeline.tsx
+++ src/components/home/Timeline.tsx
@@ -5,23 +5,81 @@ import './style.css';
 import Image from "next/image";
 import { Button } from "../ui/Button";
 
+interface TimelineProps {
+    name: string
+    description: string
+    startDate: Date
+    endDate: Date
+    img: string
+    rsvp?: string 
+
+}
 export const Timeline: React.FC = () => {
     const [index, setIndex] = useState<number>(0);
 
-    const TIMELINES = [
-        { date: "1 Jan 2024", name: "Libur" },
-        { date: "1 Jan 2024", name: "Libur" },
-        { date: "1 Jan 2024", name: "Libur" },
-        { date: "1 Jan 2024", name: "Libur" },
-        { date: "1 Jan 2024", name: "Libur" },
-        { date: "1 Jan 2024", name: "Libur" },
-        { date: "1 Jan 2024", name: "Libur" },
+    const TIMELINES: TimelineProps[] = [
+        {
+            name: "Welcoming MaBa Part 1",
+            description: "Welcoming MaBa Part 1 merupakan sebuah acara untuk mengenal dan saling berkenalan dengan mahasiswa baru yang masuk melalui jalur SNBP. Sebagai mahasiswa baru yang belum familiar dengan lingkungan UI, pada acara ini akan diperkenalkan lingkungan-lingkungan di UI seperti ada fakultas apa saja dan sarana prasarana apa saja yang disediakan oleh UI.",
+            startDate: new Date(2024, 4, 11),
+            endDate: new Date(2024, 4, 11),
+            img: "",
+        },
+        {
+            name: "Pre-event",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 7, 3),
+            endDate: new Date(2024, 7, 3),
+            img: "",
+        },
+        {
+            name: "Deadline Insight Hunting",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 7, 16),
+            endDate: new Date(2024, 7, 16),
+            img: "",
+        },
+        {
+            name: "Deadline Fossib",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 7, 29),
+            endDate: new Date(2024, 7, 29),
+            img: "",
+        },
+        {
+            name: "Deadline Networking",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 7, 31),
+            endDate: new Date(2024, 7, 31),
+            img: "",
+        },
+        {
+            name: "Deadline Mentoring",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 8, 11),
+            endDate: new Date(2024, 8, 11),
+            img: "",
+        },
+        {
+            name: "Deadline KMBUI Explorer",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 8, 11),
+            endDate: new Date(2024, 8, 11),
+            img: "",
+        },
+        {
+            name: "Closing PPMB",
+            description: "Lorem ipsum.",
+            startDate: new Date(2024, 8, 21),
+            endDate: new Date(2024, 8, 21),
+            img: "",
+        },
     ];
 
     const timelineWidth = TIMELINES.length * 180;
 
     return (
-        <div className='flex flex-col items-center gap-3 lg:gap-7'>
+        <div className='flex flex-col items-center gap-5'>
             <div className='gap-3 flex text-2xl lg:text-6xl font-semibold items-center'>
                 <span className='text-ppmb-blue-600'>TIMELINE</span>
                 <span>Kegiatan</span>
@@ -35,7 +93,7 @@ export const Timeline: React.FC = () => {
                     {TIMELINES.map((timeline, key) => (
                         <div key={key} className={`${key % 2 === 1 ? 'self-end' : 'self-start'} li flex flex-col items-center min-w-[180px]`}>
                             <div className="flex flex-col px-3 py-2 w-full rounded-xl items-center hover:bg-ppmb-100 hover:bg-opacity-70 cursor-pointer">
-                                <div className="bg-gradient-to-r from-ppmb-blue-700 to-ppmb-blue-500 rounded-xl px-3 py-1 text-sm font-medium text-ppmb-000 min-w-[110px] justify-center flex">{timeline.date}</div>
+                                <div className="bg-gradient-to-r from-ppmb-blue-700 to-ppmb-blue-500 rounded-xl px-3 py-1 text-sm font-medium text-ppmb-000 min-w-[110px] justify-center flex">{timeline.startDate == timeline.endDate ? "" : ""}</div>
                                 <span className="font-semibold text-xl text-ppmb-800">{timeline.name}</span>
                             </div>
                         </div>
