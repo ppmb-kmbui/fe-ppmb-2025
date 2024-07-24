4354c4fc835342ed6d48f7873d8435d45445b78f
sunsetheus <williamsalimrjs@gmail.com>
2024-07-25 03:54:23 +0700

    feat: implement tailwind-variant for standard input

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const loginFormSchema = z.object({
     email: z.string().email({ message: "Masukkan email yang valid!" }),
@@ -44,10 +45,10 @@ const LoginPage: React.FC = () => {
 
             <div className="flex flex-col-reverse items-center justify-center md:flex-row md:justify-evenly px-5 md:px-[60px] gap-5 h-full">
                 <div className="w-full flex flex-col font-medium gap-5 items-center justify-center">
-                    <Input {...register("email")} label="Email" placeholder="Kocite" setValue={setEmail} icon={<HiMail />}/>
-                    <Input {...register("password")} label="Password" placeholder="password" setValue={setPassword} icon={<HiLockOpen />}/>
+                    <Input {...register("email")} placeholder="Masukkan email kamu" icon={<HiMail />} label="Email" />
+                    <Input {...register("password")} placeholder="Masukkan password kamu" icon={<HiLockOpen />} label="Password" />
 
-                    <div className="flex items-center flex-col gap-2">
+                    <div className="flex items-center flex-col gap-2 mt-2">
                         <Button label="Masuk" handleClick={handleLogin} variant="lg"/>
                         <span className="font-medium">Belum memiliki akun? <span className="text-ppmb-blue-500 font-semibold hover:text-ppmb-blue-700 cursor-pointer hover:underline decoration-2" onClick={() => router.push("/signup")}>Buat Akun</span></span>
                     </div>   
@@ -68,7 +69,3 @@ const LoginPage: React.FC = () => {
 }
 
 export default LoginPage;
-
-function zodResolver(loginFormSchema: z.ZodObject<{ email: z.ZodString; password: z.ZodString; }, "strip", z.ZodTypeAny, { email: string; password: string; }, { email: string; password: string; }>): import("react-hook-form").Resolver<{ email: string; password: string; }, any> | undefined {
-    throw new Error("Function not implemented.");
-}
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,30 +2,51 @@
 
 import { useState } from "react"
 import { HiEye, HiEyeOff, HiUser } from "react-icons/hi"
+import { tv } from "tailwind-variants"
 
 interface InputProps {
     label?: string
     placeholder: string
-    setValue: (value: string) => void
     icon?: React.ReactNode
-    type?: "normal" | "rounded"
+    leftIcon?: React.ReactNode
+    variant?: "standard" | "rounded"
+    size?: "md" | "xl";
+    
 }
 
+const inputVariants = tv({
+    base: "flex flex-row border-[2px] border-ppmb-blue-600 text-ppmb-800 py-[5px] bg-ppmb-000 items-center",
+    variants: {
+        variant: {
+            standard: "rounded-md px-3 w-full",
+            rounded: "rounded-3xl px-5" 
+        },
+        size: {
+            md: "text-lg",
+            xl: ""
+        }
+    },
+    defaultVariants: {
+        variant: "standard",
+        size: "md"
+    }
+})
+
 export const Input: React.FC<InputProps> = ({
-    label, placeholder, setValue, icon, type="normal"
+    label, placeholder, icon, variant, size
 }) => {
     const [isVisible, setIsVisible] = useState<boolean>(false);
 
     return (
-        <div className={`${type == "rounded" ? "min-w-[170px] md:w-[500px] lg:max-w-[850px] lg:w-full" : "w-full"} flex flex-col md:text-lg text-ppmb-800`}>
-            {type == "normal" && <div className="flex flex-row items-center gap-1 ">
+    
+        <div className="flex flex-col w-full">
+            <div className="flex flex-row items-center gap-1">
                 <div className="text-[18px]">{icon}</div>
-                <text className="font-medium">{label}</text>
-            </div>}
-
-            <div className={`${type == "rounded" ? "rounded-3xl px-3 md:px-5 flex-row gap-2 md:gap-3 items-center" : "rounded-md px-3"} items-center border-ppmb-blue-600 bg-ppmb-000 border-[2px] py-[5px] flex flex-row`}>
-                {type == "rounded" && <div className="text-lg md:text-xl">{icon}</div>}
-                <input className="w-full bg-ppmb-000 focus:outline-none placeholder:font-normal placeholder:text-ppmb-400" type={label?.split(" ").includes("Password") && !isVisible ? "password" : "text"} placeholder={placeholder} onChange={(e) => setValue(e.target.value)}/>
+                <text className="font-medium lg:text-lg">{label}</text>
+            </div>
+            
+            <div className={inputVariants({ variant, size })}>
+                <input className="bg-ppmb-000 focus:outline-none placeholder:font-normal w-full" placeholder={placeholder}/>
                 {label?.split(" ").includes("Password") && <button className="pl-3" onClick={() => setIsVisible(!isVisible)}>{isVisible ? <HiEye /> : <HiEyeOff />}</button>}
             </div>
         </div>
