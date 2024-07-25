4846e605dd80393ffda4a0f3b967f00bf5f377f5
sunsetheus <williamsalimrjs@gmail.com>
2024-07-25 22:44:02 +0700

    fix: fix dropdown z index overlap

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -38,7 +38,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
 
             <div className={`${isOpen ? "rounded-t-md" : "rounded-md"} border-[2px] border-ppmb-blue-600 relative`} ref={ref}>
                 <div className="flex flex-row items-center justify-between px-3 py-[5px] cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-                    <div className={`${getDisplay(dropdownValue) == null && "text-ppmb-300"} font-normal`}>
+                    <div className={`${getDisplay(dropdownValue) == null && "text-ppmb-300 select-none"} font-normal`}>
                         {getDisplay(dropdownValue) == null ? `Pilih ${label}` : getDisplay(dropdownValue)}
                     </div>
                     <div className={`transform ${isOpen && 'rotate-180'} transition-transform duration-500 ease-in-out`}>
@@ -49,7 +49,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
                 <div className={`${isOpen ? "flex" : "hidden"} flex-col absolute top-[105%] w-full items-center overflow-y-auto max-h-[30vh] rounded-b-md transition-all duration-300 font-medium`}>
                     {options.map(({display, value}, key) => (
                         <div key={key} className="flex flex-col w-full text-[14px]">
-                            <div className={`${value != dropdownValue ? "block" : "hidden"} flex flex-row cursor-pointer bg-white hover:bg-stonks-100 gap-2 px-3 py-2 items-center font-medium`} onClick={() => {setDropdownValue(value), setIsOpen(false)}}>
+                            <div className={`${value != dropdownValue ? "block" : "hidden"} flex flex-row cursor-pointer bg-white hover:bg-stonks-100 gap-2 px-3 py-2 items-center font-medium z-50`} onClick={() => {setDropdownValue(value), setIsOpen(false)}}>
                                 {display}
                             </div>
                         </div>
