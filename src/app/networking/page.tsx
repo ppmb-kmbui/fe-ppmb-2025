18b99b9c2d6d97ab4ebe19a12111d9d31b252741
sunsetheus <williamsalimrjs@gmail.com>
2024-07-26 19:05:28 +0700

    style: adjust gap and text size for mobile view for networking page

--- src/app/networking/page.tsx
+++ src/app/networking/page.tsx
@@ -57,32 +57,32 @@ const NetworkingPage: React.FC = () => {
 
     return (
         isFetching ? <LoadingScreen /> :
-        <div className="min-h-screen flex flex-col gap-10 pb-10">
+        <div className="min-h-screen flex flex-col gap-6 md:gap-8 lg:gap-10 pb-10">
             <Header label="Networking" subLabel="KMBUI" />
 
-            <div className="px-5 md:pl-[40px] lg:pl-[60px]">
+            <div className="px-3 md:px-5 md:pl-[40px] lg:pl-[60px] flex flex-col gap-1 lg:gap-3">
                 <text className="text-[27px] leading-[1.6] font-semibold">Menunggu Persetujuan</text>
 
-                <div className="grid grid-cols-2 md:flex md:flex-row md:overflow-x-auto md:max-w-[84vw] lg:max-w-[89vw] items-center gap-5 scrollbar-hide py-3 pr-3">
-                    
+                <div className={`${menungguPersetujuanFriends.length == 0 ? "hidden" : "grid md:flex"} grid-cols-2 md:flex-row md:overflow-x-auto md:max-w-[84vw] lg:max-w-[89vw] items-center gap-3 lg:gap-5 scrollbar-hide lg:pr-3`}>
                     {menungguPersetujuanFriends.map((friend, key) => (
                         <UserCard key={key} {...friend} onAccept={() => handleAccept(friend)} onReject={() => handleReject(friend.id)}/>
                     ))}
-
-                    <text className={`${menungguPersetujuanFriends.length == 0 ? "flex" : "hidden"} text-lg italic w-full text-ppmb-500`}>Tidak ada permintaan pertemanan :(</text>
                 </div>
+
+                <text className={`${menungguPersetujuanFriends.length == 0 ? "flex" : "hidden"} lg:text-lg italic w-full text-ppmb-500`}>Tidak ada permintaan pertemanan :(</text>
             </div>
 
-            <div className="pl-[40px] lg:pl-[60px]">
+            <div className="px-3 md:px-5 md:pl-[40px] lg:pl-[60px] flex flex-col gap-1 lg:gap-3">
                 <text className="text-[27px] leading-[1.6] font-semibold">Lanjutkan Networking</text>
 
-                <div className="flex flex-row overflow-x-auto max-w-[84vw] lg:max-w-[89vw] items-center gap-5 scrollbar-hide py-3 pr-3">
+                <div className={`${lanjutkanNetworkingFriends.length == 0 ? "hidden" : "grid md:flex"} gap-3 lg:gap-5 grid-cols-2  md:flex-row md:overflow-x-auto md:max-w-[84vw] lg:max-w-[89vw] items-center scrollbar-hide lg:pr-3`}>
                     {lanjutkanNetworkingFriends.map((friend, key) => (
                         <UserCard key={key} {...friend}/>
                     ))}
-
-                    <text className={`${lanjutkanNetworkingFriends.length == 0 ? "flex" : "hidden"} text-lg italic w-full text-ppmb-500`}>Tidak ada teman yang bisa di-networking saat ini, silahkan follow teman pada page Cari!</text>
+                    
                 </div>
+
+                <text className={`${lanjutkanNetworkingFriends.length == 0 ? "flex" : "hidden"} lg:text-lg italic w-full text-ppmb-500`}>Tidak ada teman yang bisa di-networking saat ini, silahkan follow teman pada page Cari!</text>                
             </div>
         </div>
     )
