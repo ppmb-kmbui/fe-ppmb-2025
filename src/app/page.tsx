3052fc0363b413f71ac8718f726b779fd965fd0e
NaviCo-de <aridarrell30@gmail.com>
2025-07-17 22:53:31 +0700

    Sudah validasi yey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { useAuth } from "@/context/AuthContext";
 import withAuth from "@/hoc/withAuth";
 
 const Homepage = () => {
-<<<<<<< HEAD
   const { isAuthenticated, token, isLoading, user } = useAuth();
 
   return (
