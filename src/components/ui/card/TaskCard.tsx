2ef31a1632e3c637344f4a8750251ad0d5a6d054
Thorbert Anson Shi <tobyas2005139@gmail.com>
2025-08-25 14:24:26 +0700

    Fixed self redirect bug when checking submission

--- src/components/ui/card/TaskCard.tsx
+++ src/components/ui/card/TaskCard.tsx
@@ -41,10 +41,7 @@ export const TaskCard: React.FC<TaskProps> = ({
 
   const handleSubmit = async (data: ModalData) => {
     if (data) {
-      // console.log("ini id", id);
-      // console.log("ini type", type)
       if (data.type === "file") {
-        // console.log("haiiii2")
         let cloudinarySuccessRes;
         try {
           const form = new FormData();
@@ -188,6 +185,11 @@ export const TaskCard: React.FC<TaskProps> = ({
             console.error("Error submitting assignment link");
           }
         }
+      }
+
+      if (isFinished) {
+        // Handle assignment resubmission
+        window.location.reload();
       }
     } else {
       console.error(
@@ -334,13 +336,13 @@ export const TaskCard: React.FC<TaskProps> = ({
 
           {isFinished && (
             <>
-              <a
-                className="border-neutral-dark text-neutral-dark hover:bg-neutral-light flex min-h-[27.5px] min-w-[80px] cursor-pointer items-center justify-center gap-2 rounded-lg border-2 px-5 py-[2px] text-[13px] font-medium md:text-sm"
-                href={url}
-                target="_blank"
+              <button
+                className="border-neutral-dark disabled:border-neutral-medium disabled:text-neutral-medium text-neutral-dark hover:bg-neutral-light flex min-h-[27.5px] min-w-[80px] cursor-pointer items-center justify-center gap-2 rounded-lg border-2 px-5 py-[2px] text-[13px] font-medium disabled:cursor-not-allowed disabled:hover:bg-transparent md:text-sm"
+                onClick={() => window.open(url, "_blank")}
+                disabled={url.length === 0}
               >
                 Lihat submisi
-              </a>
+              </button>
 
               <button
                 className="flex min-h-[27.5px] min-w-[80px] cursor-pointer items-center justify-center gap-2 rounded-lg bg-blue-300 px-5 py-[2px] text-[13px] text-white hover:bg-blue-200 md:text-sm"
